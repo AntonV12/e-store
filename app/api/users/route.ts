ad358a7edea94de21cfb08f731ff3244f5ffd622
AntonV <kave-12@yandex.ru>
2025-03-30 20:37:35 +0400

    commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,16 +2,12 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { fetchUsers } from "./usersController";
 
-// interface Context {
-//   params: undefined;
-// }
-
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
     const limit = parseInt(searchParams.get("limit") || "10");
 
-    if (isNaN(limit) || limit < 0) {
+    if (isNaN(limit) || limit <= 0) {
       return NextResponse.json({ error: "Invalid limit value" }, { status: 400 });
     }
 
